ba79195ff1497eb4a9f7ce5f91ae2e824e5fad86
Tearzz <benjamin.stigsen@gmail.com>
2019-04-08 22:33:07 +0200

    Added example API command and file load response

--- twitch_commands.rb
+++ twitch_commands.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 require_relative("configurator.rb")
 
 def twitch_command(user, command)
@@ -7,18 +8,48 @@ def twitch_command(user, command)
 	response = 
 	case command.split(" ").first
 
+	# Regular example
 	when "ping"
 		ping(user)
+
+	# File example
+	when "from_file"
+		from_file("example.txt")
+
+	# API example (https://docs.decapi.me/twitch)
+	when "uptime"
+		uptime().string	# Convert StringIO to String
+
+	# Default
 	else
 		nil
+
 	end
 
+	# Send chat message if there is a response
 	if response != nil
 		puts("#{" " * (10 + user.length + 1)} #{"\\------".bold.blue}: #{response.yellow}")
 		send_msg(response)
 	end
 end
 
+# File Commands
+def from_file(file_name)
+
+	return open("./responses/#{file_name}").read
+end
+
+# Regular Commands
 def ping(user)
 	return "pong #{user}!"
+end
+
+# API Commands
+def uptime(def_msg: "")
+
+	if def_msg != ""
+		return open("https://decapi.me/twitch/uptime/#{$channel}?offline_msg=#{def_msg}")
+	else
+		return open("https://decapi.me/twitch/uptime/#{$channel}")
+	end
 end
